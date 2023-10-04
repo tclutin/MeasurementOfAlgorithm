2947b240633eb188fc92d9b942fe0c7d57adaa17
Lutin <tclutin01@gmail.com>
2023-10-04 15:21:30 +0500

    fix

--- Algorithms/Others/ClassicQuickPowAlgorithm.cs
+++ Algorithms/Others/ClassicQuickPowAlgorithm.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab1_algorithm.Algorithms.Sort
+namespace Lab1_algorithm.Algorithms.Others
 {
     public class ClassicQuickPowAlgorithm : IAlgorithmInt<int>
     {
